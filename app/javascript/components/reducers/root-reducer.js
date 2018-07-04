6a3a7031e2481da30de8c3d5307ce6c14b05c64a
TheLocust3 <jake.kinsella@gmail.com>
2018-07-03 23:05:33 -0400

    Remove old devise stuff and link stylesheets

--- app/javascript/components/reducers/root-reducer.js
+++ app/javascript/components/reducers/root-reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
-import { userReducer } from './user-reducer'
-
 const reducer = combineReducers({
-    users: userReducer
+    
 });
 
 export default reducer;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,6 @@ import thunkMiddleware from 'redux-thunk';
 import reducer from '../components/reducers/root-reducer';
 
 import Index from '../components/containers/Index';
-import SignInContainer from '../components/containers/users/SignInContainer';
-import SignUpContainer from '../components/containers/users/SignUpContainer';
-import ForgotPasswordContainer from '../components/containers/users/ForgotPasswordContainer';
-import ResetPasswordContainer from '../components/containers/users/ResetPasswordContainer';
-import EditUserContainer from '../components/containers/users/EditUserContainer';
 
 $.ajaxSetup({
     headers: {
@@ -43,11 +38,6 @@ class Base extends React.Component {
 
                             <Switch>
                                 <Route exact path='/' component={Index} />
-                                <Route exact path='/sign_in' component={SignInContainer} />
-                                <Route exact path='/sign_up' component={SignUpContainer} />
-                                <Route exact path='/forgot_password' component={ForgotPasswordContainer} />
-                                <Route exact path='/reset_password' component={ResetPasswordContainer} />
-                                <Route exact path='/users/edit' component={EditUserContainer} />
                             </Switch>
                         </div>
                     </Router>
