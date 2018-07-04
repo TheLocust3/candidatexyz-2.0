1466aa615e8ccb3bb978198f4f48233e524c7f80
TheLocust3 <jake.kinsella@gmail.com>
2018-07-03 23:14:07 -0400

    Add routes and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root to: 'root#index'
 
+  namespace :api do
+    resources :content, param: :identifier
+    patch 'refresh_campaign_id' => 'content#refreshCampaignId'
+    
+    get 'posts/:post_type' => 'posts#index'
+    get 'posts/:post_type/:url' => 'posts#show'
+    post 'posts' => 'posts#create'
+    patch 'posts/:id' => 'posts#update'
+    delete 'posts/:post_type/:url' => 'posts#destroy'
+  end
+
   get '*path', to: 'root#index', :constraints => lambda{|req| req.path !~ /\.(css|min.css|min.js)$/ }
 end
